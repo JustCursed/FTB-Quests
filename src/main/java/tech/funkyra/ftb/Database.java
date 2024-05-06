3567172d16b064043c261a5102210770465b212e
JustCursed <mytojimbyylabu@gmail.com>
2024-05-06 19:57:20 +0300

    mongo starts

---
+++ src/main/java/tech/funkyra/ftb/Database.java
@@ -0,0 +1,12 @@
+package tech.funkyra.ftb;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class Database {
+	private static final ConnectionString MongoUri = new ConnectionString("mongodb://user:passwd@127.0.0.1:1224/");
+	public static final MongoClient mongo = MongoClients.create(MongoUri);
+	public static final MongoDatabase ftbDb = mongo.getDatabase("ftb");
+}
---
+++ src/main/java/tech/funkyra/ftb/handlers/PlayerLeft.java
@@ -0,0 +1,17 @@
+package tech.funkyra.ftb.handlers;
+
+import com.feed_the_beast.ftblib.lib.data.Universe;
+import com.feed_the_beast.ftbquests.util.ServerQuestData;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+
+public class PlayerLeft {
+	@SubscribeEvent
+	public void onPlayerLeave(PlayerEvent.PlayerLoggedOutEvent e) {
+//		ServerQuestData.get();
+//		Universe.get().getTeams().stream().filter((el) -> el.get);
+//		get(e.player.getName());
+//		Universe.get().getPlayer(e.player.getUniqueID()).getData();
+//		writeData()
+	}
+}
